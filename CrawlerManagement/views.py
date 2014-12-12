9a97a49627781c793a26afd56fa6663f966b1e72
Michael Ishmael <michaeli@perfectinfo.com>
2014-12-12 12:26:40 +0000

    Introduced some structure and static files

--- CrawlerManagement/views.py
+++ CrawlerManagement/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    companies = ['123', '456', '789']
+    context = {'companies': companies};
+    return render(request, 'index.html', context)
